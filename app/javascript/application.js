a031742ce33ed12c124ce1af338b343a922ccaf4
amanda rojter <rojteramanda@gmail.com>
2022-08-18 16:26:09 +0100

    validations completed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Entry point for the build script in your package.json
+import "bootstrap";
 import "@hotwired/turbo-rails"
 import "./controllers"
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,13 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# #
+# puts 'deleting'
+# Movie.destroy_all if Rails.env.development?
+
+# Movie.create!(
+#   title: "Wonder Woman 1984"
+#   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s!"
+#   poster_url: "http:/...."
+#   rating: "6.9"
+# )
